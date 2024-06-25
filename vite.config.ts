9baa1100723ec00d78dc78cfa563aa639f539f10
shadow <cwizard493@gmail.com>
2024-06-25 09:32:33 +0900

    chore: installing npm modules and ts/vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@icons': '/src/icons',
+      '@types': '/src/types',
+      '@context': '/src/context',
+      '@components': '/src/components',
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://api.weatherstack.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
